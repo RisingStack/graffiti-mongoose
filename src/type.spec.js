c64a2482bcb148095df057eb8fbeca5b23972a93
Peter Marton <peter@risingstack.com>
2015-07-31 06:34:38 +0200

    test(type): cover type with tests

--- src/type.spec.js
+++ src/type.spec.js
@@ -1,6 +1,47 @@
 import {expect} from 'chai';
 import {getTypes} from './type';
+import {GraphQLString} from 'graphql/type';
+
+var field = require('./field');
 
 describe('type', () => {
-  it('should get types properly');
+  it('should get types for grffiti model properly', function() {
+    this.sandbox.stub(field, 'getField').returns({
+      type: GraphQLString
+    });
+
+    let models = [{
+      name: 'Foo',
+      fields: [{
+        name: 'aa',
+        path: 'aa',
+        instance: 'String'
+      }, {
+        name: 'bb',
+        path: 'bb',
+        instance: 'String'
+      }]
+    }];
+
+    let types = getTypes(models);
+
+    expect(types).to.be.eql({
+      Foo: {
+        _interfaces: [],
+        _typeConfig: {
+          fields: {
+            aa: {
+              type: GraphQLString
+            },
+            bb: {
+              type: GraphQLString
+            }
+          },
+          name: 'Foo'
+        },
+        description: undefined,
+        name: 'Foo'
+      }
+    });
+  });
 });
