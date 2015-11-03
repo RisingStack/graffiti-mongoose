6917d92d046c12af313737ca0305fe905abb8a36
Andras Toth <andras.toth93@gmail.com>
2015-11-03 14:17:07 +0100

    refactor(example): use latest graffiti

--- example/app.js
+++ example/app.js
@@ -1,8 +1,7 @@
 import koa from 'koa';
-import serve from 'koa-static';
 import mongoose from 'mongoose';
 import graffiti from '@risingstack/graffiti';
-import graffitiMongoose from '../src';
+import {getSchema} from '../src';
 
 import User from './user';
 
@@ -11,38 +10,41 @@ const port = process.env.PORT || 8080;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/graphql');
 
 // Generate sample data
-User.remove();
-const users = [];
-for (let i = 0; i < 100; i++) {
-  users.push(new User({
-    name: `User${i}`,
-    age: i,
-    createdAt: new Date() + i * 100,
-    friends: users.map((i) => i._id),
-    nums: [0, i],
-    bools: [true, false],
-    strings: ['foo', 'bar'],
-    removed: false,
-    body: {
-      eye: 'blue',
-      hair: 'yellow'
-    }
-  }));
-}
+User.remove().then(() => {
+  const users = [];
+  for (let i = 0; i < 100; i++) {
+    users.push(new User({
+      name: `User${i}`,
+      age: i,
+      createdAt: new Date() + i * 100,
+      friends: users.map((i) => i._id),
+      nums: [0, i],
+      bools: [true, false],
+      strings: ['foo', 'bar'],
+      removed: false,
+      body: {
+        eye: 'blue',
+        hair: 'yellow'
+      }
+    }));
+  }
+  User.create(users);
+});
 
-User.create(users);
+const schema = getSchema([User]);
 
 // Set up example server
 const app = koa();
 
 // attach graffiti-mongoose middleware
 app.use(graffiti.koa({
-  prefix: '/graphql',
-  adapter: graffitiMongoose,
-  models: [User]
+  schema
 }));
 
-app.use(serve(__dirname + '/dist'));
+// redirect all requests to /graphql
+app.use(function *redirect() {
+  this.redirect('/graphql');
+});
 
 app.listen(port);
 
--- example/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf dist/ && mkdir -p dist/ &&
-cp node_modules/graphiql/graphiql.css dist/graphiql.css &&
-cp node_modules/graphiql/graphiql.min.js dist/graphiql.min.js &&
-cp node_modules/react/dist/react.min.js dist/react.min.js &&
-cat index.html > dist/index.html
